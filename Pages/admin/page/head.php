70a33b3011f30b750772c11c589b6f9f10886522
crystalg1996 <crystalgzxcvbnm@gmail.com>
2022-12-05 22:32:44 +0800

    update

--- Pages/admin/page/head.php
+++ Pages/admin/page/head.php
@@ -18,7 +18,6 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
   <link rel="icon" type="image/png" href="./assets/img/pic5.png">
   <title>
     Palas Resort | Dashboard
